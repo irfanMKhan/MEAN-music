fe374ba072e5346251355df695bd90a445a57948
irfanMKhan <irfanupdate@yahoo.com>
2024-08-13 08:10:37 -0500

    Add processOn

---
+++ api/processOn.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const util = require("util");
+
+const callbackify = util.callbackify;
+
+const connectionClose_Callbackified = callbackify((message) => {
+  console.log(message);
+  mongoose.connection.close();
+});
+
+process.on(process.env.PROCESS_SIGINT, () => {
+  connectionClose_Callbackified(process.env.MESSAGE_SIGINT);
+  process.exit(0);
+});
+
+process.on(process.env.PROCESS_SIGTERM, () => {
+  callbackified_ConnectionClose(process.env.MESSAGE_SIGTERM);
+  process.exit(0);
+});
+
+process.once(process.env.PROCESS_SIGUSR2, () => {
+  callbackified_ConnectionClose(process.env.MESSAGE_SIGUSR2);
+  process.exit(0);
+});
