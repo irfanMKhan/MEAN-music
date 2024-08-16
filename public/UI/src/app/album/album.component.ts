d78c90b0065cfe7d7c35b87954af3b45b215f9b6
irfanMKhan <irfanupdate@yahoo.com>
2024-08-16 11:04:28 -0500

    Add song html

--- public/UI/src/app/album/album.component.ts
+++ public/UI/src/app/album/album.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+
+import { AlbumDataService } from '../album-data.service';
 
 @Component({
   selector: 'app-album',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './album.component.html',
-  styleUrl: './album.component.css'
+  styleUrl: './album.component.css',
 })
-export class AlbumComponent {
+export class AlbumComponent implements OnInit {
+  album:any = {};
 
+  constructor(
+    private location: Location,
+    private dataService: AlbumDataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const userid = this.route.snapshot.params['id'];
+    this.getData(userid);
+  }
+
+  getData(id: string): void {
+    this.dataService.getById(id).subscribe((data) => {
+      this.album = data;
+      console.log(data);
+      
+    });
+  }
+
+  register(form: NgForm){
+    console.log("register");
+    console.log(form.value);
+  }
+
+  onBack(): void {
+    this.location.back();
+  }
 }
---
+++ public/UI/src/app/song/song.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song',
+  standalone: true,
+  imports: [],
+  templateUrl: './song.component.html',
+  styleUrl: './song.component.css'
+})
+export class SongComponent {
+
+}
